966ca7c65b5f0d6d1c45ec8393e710a692a382ff
F-FFox <pino.thormann@outlook.com>
2024-05-31 18:52:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,138 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+print("You have arrived on the island with your boat and have started to make your way through the thick jungle you encounter.\nYou discover a narrow path of someone has cleared before you.\nYou follow the path until...\nYou reach a fork.\n\nYou look left and it looks like the path leads deeper into the dark jungle.\nA cold shiver runs down your spine as you think of taking this path\nas you are convinced you just heared a deep and dark growl.\n\nYou turn your head to the other side and find the path leading\nto a beautiful flowery meadow.\nYou hear birds chirping, you see busy bees flying around and you are convinced\nyou just saw a bunny poking its little head and ears though the flowers and tall grass.\nThe warmth fills your heart.\n\n")
+
+print('''
+   /\ /|
+   \ V/
+   | "")
+   /  |
+  /  \\    
+*(__\_\
+''')
+
+Q1 = input("What do you do?\n")
+if (Q1 == "turn right") or (Q1 == "Turn right") or (Q1 == "I turn right") or (Q1 == "right") or (Q1 == "Right") or (Q1 == "r") or (Q1 == "R") or (Q1 == "go right") or (Q1 == "Go right"):
+          print("Of cause you turn right.\nRight is safe, its warm, its bright!\nThere are bunnies here. Why youldn`t you turn right?\n\nYou stroll through the meadow, feeling the warmth of the sun\nagainst your skin.\nYou feel at peace.\nYou are so spellbound by the beauty of this place that you have forgotten\nwhat lead you here in the first place.\n\nJust as you are getting used to the new environment,\nyou hear a hurry through the grass infront of you.\nsomething is making its way through the tall grass in your direction!\nYou snap out of your daydream and instinctively grab for your sword.\nHowever before your fingers grasp the handle of your sword\nthe \"thing\" charging in your direction\nleaps out from under the grass right at your neck...\n\nBefore you can even react, the bunny you saw frolicking between the flowers a mere seconds ago\nhas now critically wounded you.\nThe murderous psychopathic bunny stares you down,\nyour blood dripping from its furry white mouth,\nas everything turns dark.\n\nYou have been defeated.\n\n")
+          print('''
+           //
+         _//
+        oo ~~-_
+   ___m<___m___~.___            
+   _|__|__|__|__|__|            
+   |__|__|__|__|__|_           
+   ''')
+          print("GAME OVER")
+elif (Q1 == "turn left") or (Q1 == "Turn left") or (Q1 == "I turn left") or (Q1 == "left") or (Q1 == "left") or (Q1 == "l") or (Q1 == "l") or (Q1 == "go left") or (Q1 == "Go left"):
+          print("You are here for an adventure!\nYou are here to find the treasure!\nYou are brave and not afraid of whatever lies beyond!\n\nYou make your way through the ever thickening jungle\nwielding your sword to free the path from any shrubbery in your way.\n\nAfter what seams like an eternity, the jungle becomes less dense.\nSuddenly the path ends before a running stream.\nStill focussed on the task of freeing the path, you notice the path ending in the last possible moment\nand almost fall into the water.\n\nThe stream is far to wide to simply jump over it.\nYou could try to swim across it, however the flow is quite fast\nand the water too mirky to assess its deapth.\nYou could also cut down a tree with your sword and use it\nas a makeshift bridge to cross the water.\n")
+          print('''
+        __ _.--..--._ _
+     .-' _/   _/\_   \_'-.
+    |__ /   _/\__/\_   \__|
+       |___/\_\__/  \___|
+              \__/
+              \__/
+               \__/
+                \__/
+             ____\__/___
+       . - '             ' -.
+      /                      \
+~~~~~~~  ~~~~~ ~~~~~  ~~~ ~~~  ~~~~~
+  ~~~   ~~~~~   ~!~~   ~~ ~  ~ ~ ~
+  ''')
+          Q2 = input("What do you do? Type \"cut\" to cut down a tree to cross the river or type \"swim\" to swim across...\n")
+          if (Q2 == "swim") or (Q2 == "Swim"):
+                    print("You decide to swimm across.\nHowever, you are afraid that the water is deep and your heavy armour will weigh you down.\nSo you strip down to your bare butt and plunge into the water.\n\nTo your suprize the water is not very deep and you can easily keep your head above it.\n Also the temperature of the water isn't too bad and you feel refreshed.\nAs you start to make your way to the other side, you notice something cold and slimy tough your left leg for just a moment...\n\nThere it is again! - This time on your right leg...\n\n...and again...\n\nYou slowly begin to panic as the thing starts to move upwards along your body\ntouching your hips, then your belly and then\nonly then it touches your chest,\nyou see the outline of the thing that has been touching you repeatedly.\nIts a a fish...a trout to be precise.\nIt seems friendly. Overly friendly.\nYou notice there are more, similarly lovestarved trout\n now having formed a formation around you.\nThe enamoured fish want to be as close as possible to you and begin\nto pile up on you. Desperately fighting to be closest to you.\n The weight of the shier mass of trout becomes unbareable to you and your body is pushed under the water surface.\n more and more fish arive and bury you under them.\n\nYour struggeling yields no result and soon your breath runs out.\nYou have been defeated.")
+                    print('''
+                     >(>     ><>     ><_>       ><'>
+
+                     >(>     ><>     ><_>       ><'>
+                       ><((">
+                              ><(((*>
+                    ''')
+                    print("GAME OVER")
+          elif (Q2 == "cut") or (Q2 == "Cut"):
+                    print("You search along the bank of the stram and find a palm tree which seems tall enough to reach the other side.\nWith your sword and a few precise swings you manage to cut a wedge out of the tree\nwhich now starts to creek and comes down with a racket.\nAfter the noise and the whirled up dust subside, you see\nthat the tree could not have landed in a better manner,\nreaching both sides of the stream, it forms the perfect bridge to cross it.\nAt least as perfect as you may expect under the given circumstances.\nYou carefully balance across the tree, minding each step carefully.\nJust as you are half way along, you notice bubbling and movement in the water beneath you.\nYou try to regain your focus and concentrate on making it to the other side safely\ntrying not to imagine what would have happened if you had tried to swim across.\n\nYou cross to the other side and continue on your way.\n\nAfter a few moments of joyfully marching along the path, you reach\na rock face. You notice three wooden doors set in the stone.\nEach door bears a different symbol etched into the wood.\nOne symbol reselmbles a heart.\nAnother, a treasure chest.\n The latter, a flame.\n\nThese doors seem to be the only way. The rock face is too tall to climb\nand the jungle seems too thick on either side to pass through.\n")
+                    print('''
+                          ______
+                       ,-' ;  ! `-.
+                      / :  !  :  . \\\n
+                     |_ ;   __:  ;  |
+                     )| .  :)(.  !  |
+                     |"    (##)  _  |
+                     |  :  ;`'  (_) (
+                     |  :  :  .     |
+                     )_ !  ,  ;  ;  |
+                     || .  .  :  :  |
+                     |" .  |  :  .  |
+                     |mt-2_;----.___|
+                    ''')
+                    Q3 = input("The doors are the only way.\nBehind which door could the treasure be?\nDo you choose the heart, the trasure chest or the flame?\nChoose wisely...\n")
+                    if (Q3 == "flames") or (Q3 == "Flames"):
+                               print("You open the door with the symbol resembling flames.\nThe door is unlocked and opens with a loud creeking sound.\nYou step into a pitch black room and the door slams shut behind you.\nOnly now do you notice a faint smell of something burning.\nBefore you can think of what your next step might be, you are engulfed in flames.\nYou did not choose wisely. What did you expect?\nYou have been defeated.\n")
+                               print('''
+                                   ,.   (   .      )        .      "
+                                  ("     )  )'     ,'        )  . (`     '`
+                                .; )  ' (( (" )    ;(,     ((  (  ;)  "  )"
+                                _"., ,._'_.,)_(..,( . )_  _' )_') (. _..( '..
+                               ''')
+                               print("GAME OVER")
+                    elif (Q3 == "heart") or (Q3 == "Heart"):
+                               print("You open the door with the heart symbol.\nAlthough the door is unlocked it is heavy and takes all your effort to\nopen it just enough so you can push yourself through.\nYou notice a torch on the wall and manage to light it just in time before\nthe door shuts behind you with a loud bang.\nAfter a moment your eyes have adjusted to the darkness and you notice you are in a tunnel of some sort.\nOne step after another you move along.\nEvery step echoeing in the darkness.\nYou free your path of any cobwebs as you slowly inch forward in the dim light of your torch.\n")
+                               print("As you move along, you start to notice a light at the end of the tunnel.\nThe light gets brighter with every step and before you notice you have crossed to the other side.\nYou find yourself before a seemingly neverending field.\nThick lushes green grass covers the ground.\nAs your eyes adjust to the bright light you see a goup of foxes basking in the sun.\nThey notice you and come pouncing towards you.\nThey instinctively know they can trust you.\nYou instinctively know you can trust them.\nYou ruffel their fur and boop their noses as they let out cheerful yipps.\nYou have found a home, your paradise, your treasure.\nYou have found love.\nYou have won the game.\n")
+                               print('''
+                                                          /\\\n                                     (~(\n
+                                    /\       /\          ) )     /\_/\\\n                                    )~)  ^_^ )~)  ^_^   ( _-----_(@ @)
+                                   ( _--_@ @( _--_@ @     (       \ /
+                                     /|-\\V   /|-\\V      /|/--\|\ V
+                                    " "  ""  " "  ""     " "   " "
+
+                                *  *       *  * 
+                              *      *   *      *
+                              *       * *       *
+                               *       *       *   
+                                 *           *
+                                   *       *
+                                     *   *
+                                      * *             
+                                       *
+
+                              Ich liebe Dich Bär!
+
+                              ''')
+                    elif (Q3 == "Treasure chest") or (Q3 == "treasure chest"):
+                              print("Why of cause the treasure must be behind the door with the treasure chest!\nYou try to push open the door but it will not move.\nWith all your force you throw your body against the door,\nhoping to bust it open. But no chance.\nThe door won't move.\n You are about to give up, when you notice a second sign on the door, just above the handle.\nThe sign says \"Pull\".\nYou pull on the door handle and the door swings open.\n\nYou take a moment to regain trust in yourself and step into the room.\nAs you take a few steps in the room, torches on its walls light up and you cant believe your eyes...\nGOLD! GOLD everywhere! A mound of gold coins, diamonds and gemstones lies before your feet\nThis is more than you could have ever imagined.\nYou cant believe your eyes.\n\nAs you bend down to pick up a gold coin, the door behind you slams shut.\nStartled by the noise you turn back towards the door only to notice it is locked.\n You pull and push but nothing happens.\n \"There must be another way out\" you think and turn around,\n just to find the room to be empty.\nWhere there were ungodly amounts of riches before, the room was now empty.\n The walls bear no hidden exits and as the light from the torches starts to dim down\nyou realise, that your fate has been settled.")
+                              print("GAME OVER")
+                    else:
+                              print ("Your indecisiveness has caused you to starve to death\nGAME OVER")
+          else:
+                    print("What unusual tounges you speak in. Start again and try something else.")
+else:
+          print("What unusual tounges you speak in. Start again and try something else.")
